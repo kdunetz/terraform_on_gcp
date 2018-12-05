cd3fdf88377231b5e77917fc81457c306fe908be
Kevin Dunetz <kevindunetz@kevindunetz-macbookpro.roam.corp.google.com>
2018-12-05 13:03:30 -0500

    Missed the rename of the file

---
+++ delete_tf.sh
@@ -0,0 +1,6 @@
+export USER=kevin
+export TF_CREDS=~/.config/gcloud/${USER}-terraform-admin.json
+export TF_VAR_project_name=${USER}-test-compute
+export TF_VAR_region=us-central1
+GOOGLE_APPLICATION_CREDENTIALS=$TF_CREDS
+terraform destroy -force
